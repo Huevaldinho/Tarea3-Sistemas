41350d2f0893725cfd9d9db9f4d93a1a914e8683
OBANDO ARRIETA FELIPE DE JESUS <felipeobando@estudiantec.cr>
2023-10-19 09:42:48 -0600

    Archivo primer problema

---
+++ Tarea3_1-2021035489.c
@@ -0,0 +1,7 @@
+/*
+Escriba un programa que escriba a un archivo la información de 10 empleados. 
+Luego escriba otro programa que imprima los datos de los empleados que se 
+encuentran almacenados en posiciones pares del archivo. 
+Debe usar la función fseek para buscar la información en el archivo (no se permite búsqueda secuencial).
+
+*/
